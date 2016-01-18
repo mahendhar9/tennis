4a756268f3e325a580e1425c9a2782acad42f1b6
Mahendhar <mahendarreddy@live.com>
2016-01-18 18:50:46 +0530

    Draw left paddle and animate the ball

--- js/app.js
+++ js/app.js
@@ -5,6 +5,32 @@ window.onload = function() {
   console.log('Hello');
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
-  canvasContext.fillStyle = "black";
-  canvasContext.fillRect(0,0, canvas.width, canvas.height);
+  
+
+  var fps = 1000/30; //30 frames per second
+  var ballX = 50;
+  var ballSpeed = 5;
+  setInterval(function(){
+    moveBall()
+  }, fps);
+
+  //move the ball
+  function moveBall() {
+    ballX = ballX + ballSpeed;
+
+    // Draw the canvas
+    canvasContext.fillStyle = "black";
+    canvasContext.fillRect(0,0, canvas.width, canvas.height);
+
+    // Left Paddle
+    canvasContext.fillStyle = "white";
+    canvasContext.fillRect(0, 210, 10, 90);
+
+    // Draw the ball
+    canvasContext.fillStyle = "white";
+    canvasContext.beginPath();
+    canvasContext.arc(ballX, 50, 10, 0, 2*Math.PI, true);
+    canvasContext.fill();
+  }
+
 }
