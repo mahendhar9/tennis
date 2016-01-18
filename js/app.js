3161f8bcd4d66c287ab36747077330bef81fe509
Mahendhar <mahendarreddy@live.com>
2016-01-18 19:05:09 +0530

    Make the ball bounce if it hits the walls

--- js/app.js
+++ js/app.js
@@ -6,17 +6,33 @@ window.onload = function() {
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   
-
   var fps = 1000/30; //30 frames per second
   var ballX = 50;
-  var ballSpeed = 5;
+  var ballY = 50;
+  var ballSpeedX = 5;
+  var ballSpeedY = 5;
   setInterval(function(){
     moveBall()
   }, fps);
 
   //move the ball
   function moveBall() {
-    ballX = ballX + ballSpeed;
+    ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
+
+    // Bounce back horizontally if it hits the wall
+    if(ballX > canvas.width) {
+      ballSpeedX = -ballSpeedX;
+    } else if(ballX < 0) {
+      ballSpeedX = -ballSpeedX;
+    }
+
+    // Bounce back vertically if it hits the wall
+    if(ballY > canvas.height) {
+      ballSpeedY = -ballSpeedY;
+    } else if(ballY < 0) {
+      ballSpeedY = -ballSpeedY;
+    }
 
     // Draw the canvas
     canvasContext.fillStyle = "black";
@@ -29,7 +45,7 @@ window.onload = function() {
     // Draw the ball
     canvasContext.fillStyle = "white";
     canvasContext.beginPath();
-    canvasContext.arc(ballX, 50, 10, 0, 2*Math.PI, true);
+    canvasContext.arc(ballX, ballY, 10, 0, 2*Math.PI, true);
     canvasContext.fill();
   }
 
