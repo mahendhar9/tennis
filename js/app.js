7ca74ea5b778b7c6cc075a39d7bf86fa99caa733
Mahendhar <mahendarreddy@live.com>
2016-01-18 18:00:18 +0530

    Create blank canvas

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var canvas;
+var canvasContext;
+
+window.onload = function() {
+  console.log('Hello');
+  canvas = document.getElementById('gameCanvas');
+  canvasContext = canvas.getContext('2d');
+  canvasContext.fillStyle = "black";
+  canvasContext.fillRect(0,0, canvas.width, canvas.height);
+}
